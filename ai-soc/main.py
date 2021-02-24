32fa1a321098de9616e8e78bba3405abfd9af645
Nick DeMarchis <65045396+ndemarchis@users.noreply.github.com>
2021-02-24 14:08:15 -0500

    creates graph

--- ai-soc/main.py
+++ ai-soc/main.py
@@ -1,13 +1,27 @@
 import requests
 import os
-
+import re
+import json
 import numpy as np
 import matplotlib.pyplot as plt
+
+newImgsToCheck = 25
 
 count = 0
 nsfwDict = {}
 x = []
 y = []
+
+intersectionalDict = {}
+genderDict = {}
+raceDict = {}
+
+outputFileName = "data.json"
+
+if os.path.exists(outputFileName):
+    with open(outputFileName) as f:
+        nsfwDict = json.load(f)
+
 
 def makeRequest(path):
     print(path)
@@ -21,34 +35,60 @@ def makeRequest(path):
     )
     return (r.json()["output"]["nsfw_score"])
 
-def addToArray(obj):
-    x.append(int(obj["imageId"]))
+def parseData(obj):
+
+    intersection = obj['race'] + obj['gender']
+    handleDictEntry(raceDict, obj, obj['race'])
+    handleDictEntry(genderDict, obj, obj['gender'])
+    handleDictEntry(intersectionalDict, obj, intersection)
+
+    x.append(int(re.sub("[^0-9]", "", obj["imageId"])))
     y.append(float(obj["score"]))
+
+def handleDictEntry(dict, obj, attribute):
+    if attribute not in dict:
+        dict[attribute] = [obj["score"]]
+    else:
+        dict[attribute].append(obj["score"])
+
+def createGraph(dicty):
+    data = []
+    keys = []
+    for key in dicty:
+        keys.append(key)
+        data.append(dicty[key])
+    
+    fig, ax = plt.subplots()
+    
+    plt.boxplot(data)
+    ax.set_xticklabels(keys)
+    plt.show()
 
 for root, dirs, files in os.walk("ai-soc/test images"):
     for f in files:
-        temp = (os.path.relpath(os.path.join(root, f), "."))
-        if ((".jpg" in temp) and (count < 2)):
-            request = makeRequest(temp)
-            nsfwDict[f] = {
-                'fullPath': temp,
-                'score': request,
-                'race': f[4],
-                'gender': f[5],
-                'modelId': f[7:10],
-                'imageId': f[11:14],
-                'expression': f[15:-4],
-            }
-            addToArray(nsfwDict[f])
-            count += 1
+        if f not in nsfwDict:
+            temp = (os.path.relpath(os.path.join(root, f), "."))
+            if ((".jpg" in temp) and (count < newImgsToCheck)):
+                request = makeRequest(temp)
+                nsfwDict[f] = {
+                    'fullPath': temp,
+                    'score': request,
+                    'race': f[4],
+                    'gender': f[5],
+                    'modelId': f[7:10],
+                    'imageId': f[11:14],
+                    'expression': f[15:-4],
+                }
+                # addToArray(nsfwDict[f])
+                count += 1
     
-print(nsfwDict)
-print(x)
-print(y)
+for key in nsfwDict:
+    parseData(nsfwDict[key])
 
-dt = 0.01
-t = np.arange(0, 30, dt)
+with open(outputFileName, 'w') as fp:
+    json.dump(nsfwDict, fp, indent=4)
 
-# plt.scatter(x, y, marker='o');
+createGraph(intersectionalDict)
+
 # plt.show()
-# print(makeRequest('ai-soc/test images/CFD/AF-200/CFD-AF-200-228-N.jpg'))
+# plt.scatter(x, y, marker='o');
