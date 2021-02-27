728049c77159a2b5d6a5a1d727922d0e6015f0fa
Nick DeMarchis <65045396+ndemarchis@users.noreply.github.com>
2021-02-27 15:04:51 -0500

    added UTKFace, readme

---
+++ ai-soc/README.md
@@ -0,0 +1,7 @@
+# In progress
+
+## Databases used
+
+Ma, D. S., Correll, J., & Wittenbrink, B. (2015). The Chicago face database: A free stimulus set of faces and norming data. Behavior research methods, 47(4), 1122–1135. [https://doi.org/10.3758/s13428-014-0532-5](https://doi.org/10.3758/s13428-014-0532-5)
+
+[UTKFace](https://susanqq.github.io/UTKFace/)
--- ai-soc/main.py
+++ ai-soc/main.py
@@ -5,18 +5,32 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 
-newImgsToCheck = 25
+newImgsToCheck = 1
 
 count = 0
 nsfwDict = {}
 x = []
 y = []
 
+UTKDict = {
+    "gender": {
+        "0": "M", 
+        "1": "F"
+    },
+    "race": {
+        "0": "W",
+        "1": "B",
+        "2": "A",
+        "3": "A",
+        "4": "M",
+    }
+}
+
 intersectionalDict = {}
 genderDict = {}
 raceDict = {}
 
-outputFileName = "data.json"
+outputFileName = "./ai-soc/data/data.json"
 
 if os.path.exists(outputFileName):
     with open(outputFileName) as f:
@@ -24,7 +38,6 @@ if os.path.exists(outputFileName):
 
 
 def makeRequest(path):
-    print(path)
     r = requests.post(
         "https://api.deepai.org/api/nsfw-detector",
         files={
@@ -33,6 +46,7 @@ def makeRequest(path):
         # headers={'api-key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K'}
         headers={'api-key': 'a35e4cbf-3de7-420a-8eee-16f3bbd24284'}
     )
+    print(r.json())
     return (r.json()["output"]["nsfw_score"])
 
 def parseData(obj):
@@ -69,26 +83,39 @@ for root, dirs, files in os.walk("ai-soc/test images"):
         if f not in nsfwDict:
             temp = (os.path.relpath(os.path.join(root, f), "."))
             if ((".jpg" in temp) and (count < newImgsToCheck)):
-                request = makeRequest(temp)
-                nsfwDict[f] = {
-                    'fullPath': temp,
-                    'score': request,
-                    'race': f[4],
-                    'gender': f[5],
-                    'modelId': f[7:10],
-                    'imageId': f[11:14],
-                    'expression': f[15:-4],
-                }
-                # addToArray(nsfwDict[f])
-                count += 1
+                if ("CFD" in temp):
+                    # request = makeRequest(temp)
+                    # nsfwDict[f] = {
+                    #     'database': "CFD",
+                    #     'fullPath': temp,
+                    #     'score': request,
+                    #     'race': f[4],
+                    #     'gender': f[5],
+                    #     'modelId': f[7:10],
+                    #     'imageId': f[11:14],
+                    #     'expression': f[15:-4],
+                    # }
+                    pass
+                elif ("UTKFace" in temp):
+                    close = f[:(len(f) - 31)]
+                    age = int(close[0:-4])
+                    if ((age >= 20) and (age <= 30)):
+                        request = makeRequest(temp)
+                        tempObj = {
+                            'database': "UTKFace",
+                            'fullPath': temp,
+                            'score': request,
+                            'race': UTKDict["race"][close[-1]],
+                            'gender': UTKDict["gender"][close[-3]],
+                            'age': age,
+                        }
+                        nsfwDict[f] = tempObj
+                        count += 1
     
-for key in nsfwDict:
-    parseData(nsfwDict[key])
+# for key in nsfwDict:
+#     parseData(nsfwDict[key])
 
 with open(outputFileName, 'w') as fp:
     json.dump(nsfwDict, fp, indent=4)
 
-createGraph(intersectionalDict)
-
-# plt.show()
-# plt.scatter(x, y, marker='o');
+# createGraph(intersectionalDict)
